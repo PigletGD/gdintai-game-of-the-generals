82d41c46e7506d5e15aa12b2ffa7cdde13548180
JasoSantiago <68273034+JasoSantiago@users.noreply.github.com>
2021-02-17 10:09:30 +0800

    Added Menu link and instruction menu

--- Game of the Generals/Assets/Scripts/Menus/MainMenu.cs
+++ Game of the Generals/Assets/Scripts/Menus/MainMenu.cs
@@ -6,4 +6,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame() => SceneManager.LoadScene("GameScene");
 
     public void QuitGame() => Application.Quit();
+
+    public void ViewInstructions() => SceneManager.LoadScene("InstructionsScene");
+
+    public void ReturnCall() => SceneManager.LoadScene("MainMenuScene");
 }
